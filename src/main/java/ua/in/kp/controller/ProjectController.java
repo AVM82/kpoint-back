60e11931e147a1b4412deb87c0b70c675e0939b3
Vitalik Solodovnychenko <shamansvs@gmail.com>
2024-01-26 19:49:22 +0200

    refactoring a previous commit

--- src/main/java/ua/in/kp/controller/ProjectController.java
+++ src/main/java/ua/in/kp/controller/ProjectController.java
@@ -3,12 +3,9 @@ package ua.in.kp.controller;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import ua.in.kp.dto.project.ProjectCreateRequestDto;
 import ua.in.kp.dto.project.ProjectResponseDto;
 import ua.in.kp.service.ProjectService;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor

16757b0583003e9dbc2f2ab01605094c5c0bde31
Vitaliy Lipovetskiy <vitaliy1972li@gmail.com>
2024-01-31 22:49:15 +0200

    update docker-compose

--- src/main/java/ua/in/kp/entity/ProjectEntity.java
+++ src/main/java/ua/in/kp/entity/ProjectEntity.java
@@ -10,7 +10,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -55,7 +54,6 @@ public class ProjectEntity {
     @ManyToMany(fetch = FetchType.LAZY)
     private Set<TagEntity> tags;
 
-    @Lob
     @Column(name = "logo_img_url", columnDefinition = "TEXT")
     private String logoImgUrl;
 

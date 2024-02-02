3823c1b2082f00bbb00e873e1835b1d6d11716fe
jAndrii <admin@mriytaroby.com.ua>
2024-02-02 12:34:57 +0200

    improve checkstyle.xml

--- src/main/java/ua/in/kp/config/MvcConfig.java
+++ src/main/java/ua/in/kp/config/MvcConfig.java
@@ -1,6 +1,5 @@
 package ua.in.kp.config;
 
-import java.util.Locale;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +7,8 @@ import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Locale;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
--- src/main/java/ua/in/kp/exception/GlobalExceptionHandler.java
+++ src/main/java/ua/in/kp/exception/GlobalExceptionHandler.java
@@ -1,18 +1,7 @@
 package ua.in.kp.exception;
 
-import java.net.URI;
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
@@ -23,6 +12,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ua.in.kp.locale.Translator;
+
+import java.net.URI;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 @RequiredArgsConstructor
